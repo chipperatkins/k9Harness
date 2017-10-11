825b4467758f42e4a548ee58a09b40c95b02121e
chipperatkins <pvatkins@crimson.ua.edu>
2017-10-10 20:41:27 -0500

    Added Couchbase Db Lite to project dependencies & started writing wrapper/test code

--- app/src/main/java/me/chipperatkins/k_9harness/MainActivity.java
+++ app/src/main/java/me/chipperatkins/k_9harness/MainActivity.java
@@ -1,13 +1,19 @@
 package me.chipperatkins.k_9harness;
 
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +32,56 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // create a dog
+        Map<String, Object> dog = new HashMap<String, Object>();
+        ArrayList<String> sessions = new ArrayList<String>();
+        dog.put("name", "Chester");
+        dog.put("hrThreshhold", 80);
+        dog.put("rrThreshhold", 99);
+        dog.put("ctThreshhold", 103);
+        dog.put("atThreshhold", null);
+        dog.put("sessions", sessions);
+
+        // create a session
+        Date now = new Date();
+        String nowString = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now);
+
+        Map<String, Double> heartRate = new HashMap<String, Double>();
+        heartRate.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 88.0);
+        heartRate.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 89.0);
+        heartRate.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 88.5);
+        heartRate.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 84.0);
+
+        Map<String, Double> coreTemp = new HashMap<String, Double>();
+        coreTemp.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 88.0);
+        coreTemp.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 89.0);
+        coreTemp.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 88.5);
+        coreTemp.put(DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now), 84.0);
+
+        // etc. do this for RR and AT as well
+
+        Map<String, Object> docContent = new HashMap<String, Object>();
+        docContent.put("date", DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(now));
+        docContent.put("endTime", null);
+        docContent.put("heartRate", heartRate);
+        docContent.put("coreTemp", coreTemp);
+
+        CbDatabase db = null;
+        String docId = null;
+
+        try {
+            db = new CbDatabase("testdb", getApplicationContext());
+
+            docId = db.create(docContent);
+            sessions.add(docId);
+            String dogId = db.create(dog);
+        }
+        catch (Exception e) {
+            // handle here...
+        }
+
+        Map<String, Object> hm = db.retrieve(docId);
     }
 
     @Override
