f378013f29776259794445a757929616fd28c44b
chipperatkins <pvatkins@crimson.ua.edu>
2017-11-27 21:26:48 -0600

    fixed some warnings

--- app/src/main/java/me/chipperatkins/k_9harness/Constants.java
+++ app/src/main/java/me/chipperatkins/k_9harness/Constants.java
@@ -4,18 +4,18 @@ package me.chipperatkins.k_9harness;
  * Created by patrickatkins on 10/24/17.
  */
 
-public interface Constants {
+class Constants {
     static final int VARIABLES_IN_PACKET = 5;
 
     // Message types sent from the BluetoothChatService Handler
-    public static final int MESSAGE_STATE_CHANGE = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_WRITE = 3;
-    public static final int MESSAGE_DEVICE_NAME = 4;
-    public static final int MESSAGE_TOAST = 5;
+    static final int MESSAGE_STATE_CHANGE = 1;
+    static final int MESSAGE_READ = 2;
+    static final int MESSAGE_WRITE = 3;
+    static final int MESSAGE_DEVICE_NAME = 4;
+    static final int MESSAGE_TOAST = 5;
 
     // Key names received from the BluetoothChatService Handler
-    public static final String DEVICE_NAME = "device_name";
-    public static final String TOAST = "toast";
+    static final String DEVICE_NAME = "device_name";
+    static final String TOAST = "toast";
 
 }
